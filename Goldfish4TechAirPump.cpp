d14798817e6c97a0b4e02b81be36cd4a7f32767f
goldfish4tech <91444137+goldfish4tech@users.noreply.github.com>
2024-12-06 16:47:27 +0800

    Add files via upload

---
+++ Goldfish4TechAirPump.cpp
@@ -0,0 +1,14 @@
+/*
+  Goldfish4TechAirPump.h - Library for Goldfish4Tech air pump running code.
+  Created by Goldfish4Tech Team, 2024-12-03.
+  Updated by Goldfish4Tech Team, 2024-12-03.
+  Contact us
+	Tel/WeChat: 18682388114
+	email:      goldfish4tech@goldfish4tech.com
+*/
+
+
+#include "Goldfish4TechAirPump.h"
+
+
+
---
+++ Goldfish4TechAirPump.h
@@ -0,0 +1,15 @@
+/*
+  Goldfish4TechAirPump.h - Library for Goldfish4Tech air pump running code.
+  Created by Goldfish4Tech Team, 2024-12-03.
+  Updated by Goldfish4Tech Team, 2024-12-03.
+  Contact us
+	Tel/WeChat: 18682388114
+	email:      goldfish4tech@goldfish4tech.com
+*/
+
+#ifndef Goldfish4TechAirPump_h
+#define Goldfish4TechAirPump_h
+
+
+#endif
+
